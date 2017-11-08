ece2ee8bb0f66941c9611dbd4b25ebb926e34694
pietro13 <pscotti45@gmail.com>
2017-11-08 07:49:44 +0100

    Main updated

--- Esercitazione_Github/Main.cpp
+++ Esercitazione_Github/Main.cpp
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <process.h>
 #include "Cecchi.h"
+#include "Raddi.h"
 #include "Tarchi.h"
 
 using namespace std;
@@ -19,9 +20,13 @@ int main()
 
 	StrutturaCecchi* V1_cecchi = new StrutturaCecchi;
 	StrutturaCecchi* V2_cecchi = new StrutturaCecchi;
-    Struttura_Tarchi* V1_tarchi = new Struttura_Tarchi;// creare delle vostre strutture
+	Struttura_Raddi* V1_Raddi = new Struttura_Raddi;
+	Struttura_Raddi* V2_Raddi = new Struttura_Raddi;
+    	Struttura_Tarchi* V1_tarchi = new Struttura_Tarchi;// creare delle vostre strutture
 	Struttura_Tarchi* V2_tarchi = new Struttura_Tarchi;
-
+	
+	V1_Raddi->semaphore = CreateSemaphore(NULL, 1, 1, NULL);
+	V2_Raddi->semaphore = CreateSemaphore(NULL, 1, 1, NULL);
 	V1_tarchi->sem = CreateSemaphore(NULL, 1, 1, NULL);
 	V2_tarchi->sem = CreateSemaphore(NULL, 1, 1, NULL);
 	V1_cecchi->s = CreateSemaphore(NULL, 1, 1, NULL);
@@ -31,6 +36,8 @@ int main()
 	Ins(V2_tarchi->vett);
 	Ins(V1_cecchi->v);
 	Ins(V2_cecchi->v);
+	Ins(V1_Raddi->vettore);
+	Ins(V2_Raddi->vettore);
 
 	cout << "\nInizio\n";
 
@@ -39,6 +46,8 @@ int main()
 	cout << "Vettori Cecchi non ordinati" << endl;
 	stampa(V1_cecchi->v);
 	stampa(V2_cecchi->v);
+	stampa(V1_Raddi->vettore);
+	stampa(V2_Raddi->vettore);
 	cout << endl;
 
 	system("pause");
@@ -47,7 +56,11 @@ int main()
 	HANDLE TH2_tarchi = (HANDLE)_beginthreadex(NULL, NULL, Tarchi_mythread, (void*)V2_tarchi, NULL, 0); // creare i vostri thread
 	HANDLE ThreadCecchi1= (HANDLE)_beginthreadex(NULL, NULL, ThreadCecchi, (void*)V1_cecchi, NULL, 0);
 	HANDLE ThreadCecchi2 = (HANDLE)_beginthreadex(NULL, NULL, ThreadCecchi, (void*)V2_cecchi, NULL, 0);
+	HANDLE ThreadRaddi1= (HANDLE)_beginthreadex(NULL, NULL, Raddi_Ordina, (void*)V1_Raddi, NULL, 0);
+	HANDLE ThreadRaddi2 = (HANDLE)_beginthreadex(NULL, NULL, Raddi_Ordina, (void*)V2_Raddi, NULL, 0);
 
+	WaitForSingleObject(ThreadRaddi1, Time);
+	WaitForSingleObject(ThreadRaddi2, Time);
 	DWORD Await1 = WaitForSingleObject(TH1_tarchi, Time);
 	DWORD Await2 = WaitForSingleObject(TH2_tarchi, Time);
 	WaitForSingleObject(ThreadCecchi1, INFINITE);
@@ -65,6 +78,8 @@ int main()
 	cout << "Vettori Cecchi ordinati" << endl;
 	stampa(V1_cecchi->v);
 	stampa(V2_cecchi->v);
+	stampa(V1_Raddi->vettore);
+	stampa(V2_Raddi->vettore);
 
 	system("pause");
 
@@ -76,11 +91,15 @@ int main()
 	CloseHandle(ThreadCecchi2);
 	CloseHandle(V1_cecchi->s);
 	CloseHandle(V2_cecchi->s);
+	CloseHandle(V1_Raddi->semaphore);
+	CloseHandle(V2_Raddi->semaphore);
 
 	delete V1_tarchi;
 	delete V2_tarchi;
 	delete V1_cecchi;
 	delete V2_cecchi;
+	delete V1_Raddi;
+	delete V2_Raddi;
 
 	return 0;
 }
